abfeeb651f1f034048876cddc875aa38d69afc75
fsasieta <fsasieta@bowdoin.edu>
2016-01-30 18:36:05 -0500

    some updates
    havent figured out the os bug

--- myMyoScript.lua
+++ myMyoScript.lua
@@ -1,9 +1,13 @@
+require "os"
 scriptId = 'com.github.franconoahjames.myfirstscript'  
 scriptTitle = "My First Script"  
 scriptDetailsUrl = "myo market first add"
 
 function onForegroundWindowChange(app, title)  
-        myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
+        myo.debug(" Basic onForegroundWindowChange: " .. app .. ", " .. title)
         myo.debug("works!")
+	myo.debug("works didly do!")
+	os.execute("start chrome www.facebook.com")
+	myo.debug("just used the os execute command in lua")
         return true
     end
